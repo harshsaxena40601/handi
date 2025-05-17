17f12578ad093f6da6205df25d5a1003428431b3
harshsaxena40601 <146015392+harshsaxena40601@users.noreply.github.com>
2025-05-17 14:07:05 +0530

    h

--- product-description/script.js
+++ product-description/script.js
@@ -1,5 +1,6 @@
 // API base URL constant
-const API_BASE_URL = "http://127.0.0.1:8000/api";
+// const API_BASE_URL = "http://127.0.0.1:8000/api";
+const API_BASE_URL = "https://handicraft-5708.onrender.com";
 
 // Function to get URL parameters
 function getUrlParameter(name) {
